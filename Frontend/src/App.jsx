d66f130452ced9003cdf80aadbe2c6fb7d682a3a
njrgourav11 <79019576+njrgourav11@users.noreply.github.com>
2024-04-07 23:27:49 +0530

    backend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,22 +9,30 @@ import ContactPage from './components/ContactPage/ContactPage';
 import Footer from './components/Footer/Footer.tsx';
 import Faq from './components/FaqAccordion/FaqAccordion.jsx';
 import privacy from './components/Footer/Privacy.jsx';
+import { UserProvider } from "./Context/user.context.js";
+import Login from "./components/LoginPage/Login.page";
+//import PrivateRoute from "./components/LoginPage/PrivateRoute.page";
+import Signup from "./components/LoginPage/Signup.page";
+
 function App() {
   return (
     
     <BrowserRouter>
     <Header/>
+    <UserProvider>
       <Routes>
       
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<AboutPage />} />
-        <Route path="/courses" element={<ProgramsPage />} />
+        <Route path="/course" element={<ProgramsPage />} />
         <Route path="/events" element={<EventsPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/faq" element={<Faq />} />
         <Route path="/privacy" element={<privacy />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
-
+      </UserProvider>
       <Footer />
 
     </BrowserRouter>
